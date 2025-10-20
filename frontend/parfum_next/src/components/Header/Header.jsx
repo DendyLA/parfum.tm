d4490a326fdfdb50dc0d23ffb83d7b78874c4fe8
Dawa <atayewdawlet@gmail.com>
2025-10-20 17:44:57 +0500

    add the header__top

--- frontend/parfum_next/src/components/Header/Header.jsx
+++ frontend/parfum_next/src/components/Header/Header.jsx
@@ -7,17 +7,33 @@ import styles from './Header.module.scss';
 export default function Header() {
 
 	return (
-		<header className="header">
-			<div className="header__top">
-				<div className="container">
-					<div className="features"></div>
-					<div className="actions"></div>
-					<div className="lang"></div>
+		<header className={styles.header}>
+			<div className="container">
+				<div className={styles.header__top}>
+					<div className={styles.features}>100% оригинальная продукция</div>
+					<div className={styles.actions}>
+						<ul className={styles.actions__list}>
+							<li className={styles.actions__item}><Link className="link-extra" href="">Акции</Link></li>
+							<li className={styles.actions__item}><Link className="link" href="">Доставка и оплата</Link></li>
+							<li className={styles.actions__item}><Link className="link" href="">Про магазин</Link></li>
+						</ul>
+					</div>
+					<div className={styles.lang}>
+						<ul className={styles.lang__list}>
+							<li className={styles.lang__item}>
+								<Link className="link" href="">Рус</Link>
+							</li>
+							<li className={`${styles.lang__item} ${styles.active}`}>
+								<Link className="link" href="">Ткм</Link>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div className="header__middle">
+
 				</div>
 			</div>
-			<div className="header__middle">
-
-			</div>
+			
 		</header>
 	)
 
