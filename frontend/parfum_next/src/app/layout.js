40b6559cbd9955d734ee9b1414e0184c465d84a4
Dawa <atayewdawlet@gmail.com>
2025-10-27 17:57:14 +0500

    add the favicons

--- frontend/parfum_next/src/app/layout.js
+++ frontend/parfum_next/src/app/layout.js
@@ -1,6 +1,13 @@
 import "@/styles/global.scss";
 import Header from "@/components/Header/Header"; 
 
+export const metadata = {
+  icons: {
+    icon: '/favicons/favicon.ico',
+    apple: '/favicons/apple-touch-icon.png',
+  },
+};
+
 
 
 export default function RootLayout({ children }) {
